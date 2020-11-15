e05588556bb8714d4cf40ea10c3882c178d977d6
abalo-kokou <kokouabalo@gmail.com>
2020-11-16 00:31:44 +0100

    courbe de bezier

---
+++ TP_Courbe_Bezier/Assets/Bezier_Curve_Script.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bezier_Curve_Script : MonoBehaviour
+{
+    public GameObject p0, p1, p2, p3;
+	public List<GameObject> listPointsCtrl;
+    public int numberOfPoints = 20;
+	public int nCtrlPoints = 4;
+	int ctrlPointNbr = -1;
+	LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.positionCount = nCtrlPoints;
+
+		Vector3 v0 = p0.transform.position;
+		Vector3 v1 = p1.transform.position;
+		Vector3 v2 = p2.transform.position;
+		Vector3 v3 = p3.transform.position;
+		
+		lineRenderer.SetPosition(0, v0);
+		lineRenderer.SetPosition(1, v1);
+		lineRenderer.SetPosition(2, v2);
+		lineRenderer.SetPosition(3, v3);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		drawBezier();
+		//driveCtrlPoints();
+	}
+
+	void drawBezier(){
+		lineRenderer.positionCount = numberOfPoints;
+		List<Vector3> lstPositions = new List<Vector3>();
+
+		for (int nPoint = 0; nPoint <= numberOfPoints; nPoint++){
+			float t = nPoint / (numberOfPoints - 1.0f);
+			Vector3 position = Vector3.zero;
+
+			for (int i = 0; i < 4; i++){
+				position += listPointsCtrl[i].transform.position*bersteinCurve(i,t); // somme des positions
+			}
+			lstPositions.Add(position); // Ajout des listes de position
+		}
+		lineRenderer.SetPositions(lstPositions.ToArray());
+	}
+
+	float bersteinCurve(int i, float t){
+		float b;
+
+		b = Mathf.Pow(t, i) * Mathf.Pow(1-t, nCtrlPoints -1 -i) * Factorial(nCtrlPoints -1)/(Factorial(i) * Factorial(nCtrlPoints -1 -i));
+
+		return b;
+	}
+
+	static long Factorial(long n)
+    {
+        return n > 1 ? n * Factorial(n - 1) : 1;
+    }
+
+	void driveCtrlPoints()
+    {
+		//if (Input.GetKeyDown("") { }
+
+		//transform.position += Vector3.up;
+		//transform.position += Vector3.left;
+		//transform.position += Vector3.right;
+		//transform.position += Vector3.down;
+
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+
+		if (Input.GetKeyDown(KeyCode.Alpha0))
+			ctrlPointNbr = 0;
+
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+			ctrlPointNbr = 1;
+
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+			ctrlPointNbr = 2;
+
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+			ctrlPointNbr = 3;
+		//    p3.GetComponent<GameObject>().color = Color.red;
+		//p3.color = Color.white;
+
+		if (Input.GetKeyDown(KeyCode.Space))
+			ctrlPointNbr = -1;
+		
+
+		//if (ctrlPointNbr != -1 && ctrlPointNbr != 0)
+		//	listPointsCtrl[ctrlPointNbr].transform.position += new Vector3(-x / 10f, y / 10f, 0);
+		//else
+		//	if (ctrlPointNbr == 0 && !continu)
+		//	spheres[ctrlPointNbr].transform.position += new Vector3(-x / 10f, y / 10f, 0);
+	}
+
+
+    //void setPointsOfHermiteCurve(Vector3 pv0, Vector3 pv1, Vector3 pv2, Vector3 pv3)
+    //{
+    //    float t;
+    //    Vector3 position;
+
+    //    lineRenderer.SetPosition(0, pv0);
+    //    lineRenderer.SetPosition(1, pv1);
+    //    lineRenderer.SetPosition(2, pv2);
+    //    lineRenderer.SetPosition(3, pv3);
+    //}
+}
---
+++ TP_Courbe_Bezier/Assets/Hermite_Curve.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Hermite_Curve : MonoBehaviour
+{
+	public GameObject start, startTangentPoint, end, endTangentPoint;
+
+	private Color scolor = Color.green;
+	public Color ecolor = Color.red;
+	public float width = 0.2f;
+	public int numberOfPoints = 100;
+	LineRenderer lineRenderer;
+
+	void Start()
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+		//les GameObject start, startTangentPoint, end, endTangentPoint
+		//sont directement inities dans unity par leur composant correspondant
+	}
+
+	void Update()
+	{
+		// check parameters and components
+		if (null == lineRenderer || null == start || null == startTangentPoint
+			|| null == end || null == endTangentPoint)
+		{
+			//Debug.Log("one of start, startTangentPoint, end, endTangentPoint may be null");
+			return; // no points specified
+		}
+
+		// update line renderer
+		lineRenderer.startColor = scolor;
+		lineRenderer.endColor = ecolor;
+		lineRenderer.startWidth = width;
+		lineRenderer.endWidth = width;
+		if (numberOfPoints > 1) // car il faut au moins 2 points
+		{
+			lineRenderer.positionCount = numberOfPoints; //positionCount returns the number of vertices in the line.
+		}
+
+		// set points of Hermite curve
+		Vector3 p0 = start.transform.position;
+		Vector3 p1 = end.transform.position;
+		Vector3 v0 = startTangentPoint.transform.position - start.transform.position;
+		Vector3 v1 = endTangentPoint.transform.position - end.transform.position;
+		runHermiteCurve(p0, p1, v0, v1);
+	}
+
+	void runHermiteCurve(Vector3 p0, Vector3 p1, Vector3 v0, Vector3 v1) { 
+	float u;
+	Vector3 pPosition;
+
+		for (int i = 0; i<numberOfPoints; i++)
+		{
+			u = i / (numberOfPoints - 1.0f); // (numberOfPoints - 1.0f) => intervalle entre les points ou nombre 
+												// de segments constituant la courbe 
+		 //u=0 => point de depart / u = 1 point d'arrivee
+			pPosition = (2.0f * u* u * u - 3.0f * u* u + 1.0f) * p0
+		  + (-2.0f * u* u * u + 3.0f * u* u) * p1
+		  + (u* u * u - 2.0f * u* u + u) * v0
+		  + (u* u * u - u* u) * v1;
+			lineRenderer.SetPosition(i, pPosition);
+		}
+	}
+}
